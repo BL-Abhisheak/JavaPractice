79738d645f1e14583e4c020724a1b5c0f57506f7
Abhisheak B S <abhisheak.bhaskaran@bridgelabz.com>
2025-11-28 10:59:50 +0530

    Add files via upload

---
+++ Day 24/ContactsStr.java
@@ -0,0 +1,133 @@
+package com.streams;
+
+import java.util.Objects;
+
+public class ContactsStr {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNo;
+    private  String email;
+
+    public  ContactsStr(){
+
+    }
+
+    public ContactsStr(String firstName, String lastName, String address, String city, String state, String zip, String phoneNo, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBookContacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+
+
+
+    // --------- NEW: equals() and hashCode() ----------
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ContactsStr)) return false;
+        ContactsStr other = (ContactsStr) o;
+        // equality based on firstName + lastName, case-insensitive
+        return Objects.equals(normalize(this.firstName), normalize(other.firstName)) &&
+                Objects.equals(normalize(this.lastName), normalize(other.lastName));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(normalize(firstName), normalize(lastName));
+    }
+
+    // helper to normalize strings (null-safe, lower-case trimmed)
+    private String normalize(String s) {
+        return s == null ? "" : s.trim().toLowerCase();
+    }
+}
