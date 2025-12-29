eafede66e48fe305142568bbe28520e5e49dc60b
Abhisheak B S <abhisheak.bhaskaran@bridgelabz.com>
2025-12-29 16:46:53 +0530

    Add files via upload

---
+++ Day 35/Test/JdbcrfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbcrfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcrfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
